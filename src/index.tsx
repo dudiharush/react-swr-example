e4354ca802cab7a5dc868b77cfd409212a1fde67
dudiharush <davidharush@gmail.com>
2020-06-11 22:52:28 +0300

    add react-query dev-tools

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
 import * as React from "react";
 import { render } from "react-dom";
-
+import { ReactQueryDevtools } from "react-query-devtools";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-render(<App />, rootElement);
+render(
+  <>
+    <App />
+    <ReactQueryDevtools initialIsOpen={false} />
+  </>,
+  rootElement
+);
